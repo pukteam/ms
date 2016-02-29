851eed6faaca705cf2d11aaed582bf7ac2d0331f
Yakov_work <Proglotina849@mail.ru>
2016-02-29 11:53:40 +0300

    fix model bank card

--- representation/models.py
+++ representation/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 
 class BankCard(models.Model):
-    number = models.CharField(max_length=16, null=False)
+    number = models.CharField(max_length=16, null=False, unique=True)
     surname = models.CharField(max_length=50, blank=False)
     name = models.CharField(max_length=50, blank=False)
     middle_name = models.CharField(max_length=50, null=False)
-    balance = models.FloatField(max_length=100, null=False, unique=True)
+    balance = models.FloatField(max_length=100, null=False)
     pin_code = models.CharField(max_length=4, null=False, blank=False)
     availability = models.BooleanField(null=False, default=True)
     bank_name = models.CharField(max_length=100, blank=False)
