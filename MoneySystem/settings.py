fc7fdd48db0955110004c791807fcbd3af0ca61f
Yakov_work <yakov.badygin@gmail.com>
2016-04-24 13:24:38 +0300

    static url

--- MoneySystem/settings.py
+++ MoneySystem/settings.py
@@ -110,9 +110,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-# STATIC_URL = '/static/'
-#
-# STATICFILES_DIRS = ([os.path.join(BASE_DIR, "static")])
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ([os.path.join(BASE_DIR, "static")])
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
