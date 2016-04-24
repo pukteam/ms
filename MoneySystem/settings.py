a4bb5b3958d9a7fc17578fd1aad3b451447ee916
Yakov_work <yakov.badygin@gmail.com>
2016-04-24 22:02:41 +0300

    test migration

--- MoneySystem/settings.py
+++ MoneySystem/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 from django.utils.translation import ugettext
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -92,6 +93,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
